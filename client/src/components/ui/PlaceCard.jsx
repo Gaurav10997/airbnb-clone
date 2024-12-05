618041811e341927253e78b9830fa8667fb33121
Prince Kumar Pandit <pp611223@gmail.com>
2024-12-06 02:31:45 +0530

    add function in icons and cards

--- client/src/components/ui/PlaceCard.jsx
+++ client/src/components/ui/PlaceCard.jsx
@@ -8,69 +8,97 @@ const PlaceCard = ({ place }) => {
   const [isVisibleRight, setIsVisibleRight] = useState(false);
   const [isVisibleLeft, setIsVisibleLeft] = useState(false);
   // console.log(photos.length)
-  
+
   function imageChangedRight(e) {
-      e.stopPropagation();
-      // console.log(count);
-      if(count < photos.length-1){
-      setCount((count)=> count += 1);
+    // e.stopPropagation();
+    // console.log(count);
+    if (count < photos.length - 1) {
+      setCount((count) => (count = count + 1));
       setIsVisibleRight(true);
-      }else{
-        setIsVisibleRight(false)
-      }
-      // if(count == photos.length-1) setIsVisible(false);
+    } else {
+      setIsVisibleRight(false);
+    }
+    // if(count == photos.length-1) setIsVisible(false);
   }
-  function imageChangedLeft(e){
-    e.stopPropagation();
-    if(count > 0){
-      setCount((count) => count = count - 1);
+  function imageChangedLeft(e) {
+    // e.stopPropagation();
+    if (count > 0) {
+      setCount((count) => (count = count - 1));
       // setIsVisibleLeft(true);
     }
   }
-  
-  function displayFlex(){
-    if(count < photos.length-1)
-    setIsVisibleRight(true)
-    if(count > 0)
-    setIsVisibleLeft(true)
+
+  function displayFlex() {
+    if (count < photos.length - 1) {
+      setIsVisibleRight(true);
+    } else if (count == photos.length - 1) {
+      setIsVisibleRight(false);
+    }
+    if (count > 0) {
+      setIsVisibleLeft(true);
+    } else if (count == 0) {
+      setIsVisibleLeft(false);
+    }
+
     // console.log(isVisible);
   }
-  function displayNone(){
-    
-    setIsVisibleRight(false)
-    
-    setIsVisibleLeft(false)
+  function displayNone() {
+    setIsVisibleRight(false);
+
+    setIsVisibleLeft(false);
     // console.log(isVisible);
   }
-  
-  return (<div className='flex flex-col w-full'>
-    <i onClick={imageChangedRight} onMouseOver={displayFlex} className={`material-symbols-outlined h-7 w-7 ml-64  mt-28 absolute cursor-pointer  bg-white rounded-2xl items-center justify-center ${isVisibleRight ? "flex" : "hidden"}`}>
-              chevron_right
-     </i>
-          <i onMouseOver={displayFlex} onClick={imageChangedLeft} className={`material-symbols-outlined h-7 w-7 mt-28 ml-3  absolute cursor-pointer bg-slate-50 rounded-2xl items-center justify-center ${isVisibleLeft ? "flex" : "hidden"}`}>
-            chevron_left
-          </i>
-    <Link to={`/place/${placeId}`} onMouseOver={displayFlex} onMouseLeave={displayNone} className=" flex flex-col md:m-2 xl:m-0">
-          
-      <div className="card ">
-        {photos?.[count] && (
-          
-          <img
-            src={`${photos?.[count]}`}
-            className="h-4/5 w-full rounded-xl object-cover"
-          />
-        )}
-        <h2 className="truncate font-bold">{address}</h2>
-        <h3 className="truncate text-sm text-gray-500">{title}</h3>
-        <div className="mt-1">
-          <span className="font-semibold">₹{price} </span>
-          per night
+
+  return (
+    <div className="relative flex w-full flex-col">
+      <i
+        onClick={imageChangedRight}
+        onMouseOver={displayFlex}
+        className={`material-symbols-outlined absolute ml-64 mt-28  h-7 w-7 cursor-pointer  items-center justify-center rounded-2xl bg-white/80 hover:bg-white ${isVisibleRight ? 'flex' : 'hidden'}`}
+      >
+        chevron_right
+      </i>
+      <i
+        onMouseOver={displayFlex}
+        onClick={imageChangedLeft}
+        className={`material-symbols-outlined absolute ml-3 mt-28 h-7  w-7 cursor-pointer items-center justify-center rounded-2xl bg-white/80 hover:bg-white ${isVisibleLeft ? 'flex' : 'hidden'}`}
+      >
+        chevron_left
+      </i>
+
+      <Link
+        to={`/place/${placeId}`}
+        onMouseOver={displayFlex}
+        onMouseLeave={displayNone}
+        className=" flex flex-col md:m-2 xl:m-0"
+      >
+        <div className="card ">
+          {photos?.[count] && (
+            <img
+              src={`${photos?.[count]}`}
+              className="h-4/5 w-full rounded-xl object-cover"
+            />
+          )}
+          <div className="absolute bottom-24 left-0 right-0">
+            <div className="flex h-3 items-center justify-center gap-2">
+              {photos.map((item, i) => {
+                return (
+                  <div
+                    className={`h-3 w-3 rounded-full bg-white transition-all ${count === i ? 'p-2' : 'bg-opacity-50'}`}
+                  ></div>
+                );
+              })}
+            </div>
+          </div>
+          <h2 className="truncate font-bold">{address}</h2>
+          <h3 className="truncate text-sm text-gray-500">{title}</h3>
+          <div className="mt-1">
+            <span className="font-semibold">₹{price} </span>
+            per night
+          </div>
         </div>
-      </div>
-    </Link>
-    
-  </div>
-    
+      </Link>
+    </div>
   );
 };
 
--- client/src/pages/Icons.jsx
+++ client/src/pages/Icons.jsx
@@ -1,68 +1,110 @@
-import React from 'react'
-
+import React, { useState } from 'react';
 function Icons() {
-    let arr = [
-        {
-          src: "https://a0.muscache.com/im/pictures/mediaverse/category_icon/original/3e5243c8-4d15-4c6b-97e3-7ba2bb7bb880.png",
-          name: "Icons",
-          category: "ICONS"
-        },{
-          src:"https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg",
-          name: "Amazing Views",
-          category: "AMAZING VIEWS"
-          
-        },{
-          src:"https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg",
-          name: "Farms",
-          category: "FARMS"
-        },{
-          src:"https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg",
-          name: "Amazing pools",
-          category: "AMAZING POOLS"
-        },{
-          src: "https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg",
-          name: "Domes",
-          category: "DOMES"
-        },{
-          src: "https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg",
-          name: "Beachfront",
-          category: "BEACHFRONT"
-        },{
-          src: "https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg",
-          name: "OMG!",
-          category:"OMG!"  
-        },{
-          src: "https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg",
-          name: "Countryside",
-          category:"COUNTRYSIDE"
-        },{
-          src:     "https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg",
-          name: "Rooms",
-          category: "ROOMS"
-        },{
-            src: "https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg",
-            name: "Cabins",
-            category: "CABINS"
-        }
-        
-      ];
+  const [change, setChange] = useState(false);
+  let arr = [
+    {
+      src: 'https://a0.muscache.com/im/pictures/mediaverse/category_icon/original/3e5243c8-4d15-4c6b-97e3-7ba2bb7bb880.png',
+      name: 'Icons',
+      category: 'ICONS',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/3b1eb541-46d9-4bef-abc4-c37d77e3c21b.jpg',
+      name: 'Amazing Views',
+      category: 'AMAZING VIEWS',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/aaa02c2d-9f0d-4c41-878a-68c12ec6c6bd.jpg',
+      name: 'Farms',
+      category: 'FARMS',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/3fb523a0-b622-4368-8142-b5e03df7549b.jpg',
+      name: 'Amazing pools',
+      category: 'AMAZING POOLS',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/89faf9ae-bbbc-4bc4-aecd-cc15bf36cbca.jpg',
+      name: 'Domes',
+      category: 'DOMES',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/bcd1adc0-5cee-4d7a-85ec-f6730b0f8d0c.jpg',
+      name: 'Beachfront',
+      category: 'BEACHFRONT',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/c5a4f6fc-c92c-4ae8-87dd-57f1ff1b89a6.jpg',
+      name: 'OMG!',
+      category: 'OMG!',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/6ad4bd95-f086-437d-97e3-14d12155ddfe.jpg',
+      name: 'Countryside',
+      category: 'COUNTRYSIDE',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/7630c83f-96a8-4232-9a10-0398661e2e6f.jpg',
+      name: 'Rooms',
+      category: 'ROOMS',
+      ui: false,
+    },
+    {
+      src: 'https://a0.muscache.com/pictures/732edad8-3ae0-49a8-a451-29a8010dcc0c.jpg',
+      name: 'Cabins',
+      category: 'CABINS',
+      ui: false,
+    },
+  ];
+  const [css, setCss] = useState({});
+  let ind = -1;
+
+  function changeUi(e) {
+    if (ind != -1) {
+      ind.className = `left-1/2 right-1/2    flex h-full w-full cursor-pointer px-4 text-gray-500 transition delay-75 duration-75 hover:border hover:text-black hover:shadow-md`;
+    }
+    let element = e.target;
+    if (element.localName == 'img' || element.localName == 'span') {
+      element = element.parentElement.parentElement;
+    } else if (element.localName == 'div') {
+      element = element.parentElement;
+    }
+    element.className = `left-1/2 right-1/2    flex h-full w-full cursor-pointer px-4 text-gray-500 transition delay-75 duration-75 border text-black shadow-md`;
+
+    ind = element;
+  }
+
   return (
     <>
-      <div className="border-b-2 px-16 h-20 mt-16 z-20 w-full bg-white">
-        <ul className="flex gap-0   justify-between h-full">
-          {arr.map((i) => {
-            
-            return <li className="px-4  flex    left-1/2 right-1/2 text-gray-500 hover:border-1 hover:text-black hover:shadow-md h-full w-full cursor-pointer transition delay-75 duration-75">
-              <div className="flex  flex-col items-center justify-center w-full">
-                <img src={i.src} alt="logo" width={24} height={24}/>
-                <span className=" h-fit w-fit flex flex-wrap text-xs ">{i.name}</span>
-              </div>
-            </li>;
+      <div className="z-20 mt-16 h-20  w-full border-b-2 bg-white px-16 shadow-sm">
+        <ul className="flex h-full  justify-between gap-0">
+          {arr.map((i, j) => {
+            return (
+              <li
+                className={`left-1/2 right-1/2    flex h-full w-full cursor-pointer px-4 text-gray-500 transition delay-75 duration-75 hover:border hover:text-black hover:shadow-md `}
+                onClick={changeUi}
+                id={i.name}
+              >
+                <div className="flex  w-full flex-col items-center justify-center">
+                  <img src={i.src} alt="logo" width={24} height={24} />
+                  <span className=" flex h-fit w-fit flex-wrap text-xs ">
+                    {i.name}
+                  </span>
+                </div>
+              </li>
+            );
           })}
         </ul>
       </div>
     </>
-  )
+  );
 }
 
-export default Icons
+export default Icons;
