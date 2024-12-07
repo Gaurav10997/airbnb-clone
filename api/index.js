f99aaa8748f5ed0bf1aa57a6bc0b4d241904844c
Gaurav Mandal <avdhika@gmail.com>
2024-12-07 23:45:32 +0530

    deployment changes

--- api/index.js
+++ api/index.js
@@ -108,8 +108,8 @@ app.get("/*", (req, res) => {
 });
 
 
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
+// const PORT = process.env.PORT || 3000;
+// app.listen(PORT, () => console.log(`Server is running on port ${PORT}`));
 
 module.exports = app;
 
